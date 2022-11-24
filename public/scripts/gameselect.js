aa790949804e2ccb8e104d2505419734b7e339fa
GenLancelot <stolarzk1@gmail.com>
2022-11-24 19:34:22 +0100

    second select styled

--- public/scripts/gameselect.js
+++ public/scripts/gameselect.js
@@ -1,9 +1,15 @@
 const wrapper = document.querySelector(".gameselect-wrapper"),
     selectBtn = wrapper.querySelector(".gameselect-btn"),
-    searchInp = wrapper.querySelector("input"),
+    searchInp = wrapper.querySelector(".gameinput"),
     options = wrapper.querySelector(".gameoptions");
 
+const rankwrapper = document.querySelector(".rankselect-wrapper"),
+    rankselectBtn = rankwrapper.querySelector(".rankselect-btn"),
+    ranksearchInp = rankwrapper.querySelector(".rankinput"),
+    rankoptions = rankwrapper.querySelector(".rankoptions");
+
 let games = ["GTA V","CS:GO","LeagueOfLegends","FIFA","Fortnite","Warzone", "Valorant"];
+let ranks = ["Bronze I","Bronze II","Bronze III","Bronze IV",];
 
 function addGame(selectedGame) {
     options.innerHTML = "";
@@ -14,12 +20,28 @@ function addGame(selectedGame) {
     });
 }
 addGame();
+function addRank(selectedRank) {
+    rankoptions.innerHTML = "";
+    ranks.forEach(rank => {
+        let isSelected = rank === selectedRank ? "selected" : "";
+        let li = `<li onclick="updateRankName(this)" class="${isSelected}">${rank}</li>`;
+        rankoptions.insertAdjacentHTML("beforeend", li);
+    });
+}
+addRank();
 
 function updateName(selectedLi) {
     searchInp.value = "";
     addGame(selectedLi.innerText);
     wrapper.classList.remove("active");
     selectBtn.firstElementChild.innerText = selectedLi.innerText;
+}
+
+function updateRankName(selectedLi) {
+    ranksearchInp.value = "";
+    addRank(selectedLi.innerText);
+    rankwrapper.classList.remove("active");
+    rankselectBtn.firstElementChild.innerText = selectedLi.innerText;
 }
 
 searchInp.addEventListener("keyup", () => {
@@ -34,4 +56,17 @@ searchInp.addEventListener("keyup", () => {
     options.innerHTML = arr ? arr : `<p style="margin-top: 10px;">Oops! Game not found</p>`;
 });
 
+ranksearchInp.addEventListener("keyup", () => {
+    let arr = [];
+    let searchWord = ranksearchInp.value.toLowerCase();
+    arr = ranks.filter(data => {
+        return data.toLowerCase().startsWith(searchWord);
+    }).map(data => {
+        let isSelected = data === rankselectBtn.firstElementChild.innerText ? "selected" : "";
+        return `<li onclick="updateName(this)" class="${isSelected}">${data}</li>`;
+    }).join("");
+    rankoptions.innerHTML = arr ? arr : `<p style="margin-top: 10px;">Oops! Rank not found</p>`;
+});
+
 selectBtn.addEventListener("click", () => wrapper.classList.toggle("active"));
+rankselectBtn.addEventListener("click", () => rankwrapper.classList.toggle("active"));
