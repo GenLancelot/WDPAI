884e9695b63c08d9850bffbfecb74b1f794134c8
GenLancelot <stolarzk1@gmail.com>
2022-12-09 23:30:53 +0100

    register validation

--- public/views/registration.php
+++ public/views/registration.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <head>
     <link rel="stylesheet"  type="text/css" href="public/css/style.css">
-
+    <script type="text/javascript" src="public/scripts/script.js" defer></script>
     <title>Register to Teamfinder</title>
 </head>
 <body>
@@ -14,8 +14,9 @@
             <form action="registration" method="POST">
                 <div id="loginform">
                     <h1>Register to our app</h1>
-                    <input name="email" type="text" placeholder="unnamedplayer@gmail.com">
+                    <input name="email" type="text" placeholder="unnamedplayer@gmail.com" >
                     <input name="password" type="password" placeholder="password">
+                    <input name="confirmedpassword" type="password" placeholder="confirm password">
                     <button type="submit">Next</button>
                 </div>
                 <p id="registration">Or sing in <a>here!</a></p>
